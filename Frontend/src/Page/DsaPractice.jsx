2bf6027885c5f906be78ce7477ddca3928650624
ani801 <140055484+ani801@users.noreply.github.com>
2025-07-21 01:48:31 +0530

    Update DsaPractice.jsx

--- Frontend/src/Page/DsaPractice.jsx
+++ Frontend/src/Page/DsaPractice.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useMemo, useContext } from "react";
+import { useState, useEffect, useMemo, useContext } from "react"
+  import React from 'react';
 import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import PracticeContext from "../context/PracticeContext";
