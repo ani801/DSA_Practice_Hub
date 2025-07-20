1f5e240e7c1ac689d80692a4a37da5945f0e5625
ani801 <140055484+ani801@users.noreply.github.com>
2025-07-21 01:44:32 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// vite.config.js
 export default defineConfig({
-  // ...other settings
-  server: {
-    host: true, // allow access from external sources
-    allowedHosts: ['https://dsa-practice-hub-frontend-0i71.onrender.com'],
-  },
-});
-
+  plugins: [react()],
+})
