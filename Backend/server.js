6746f437d391f4269f96d09fd835065ab1c0ce58
ani801 <140055484+ani801@users.noreply.github.com>
2025-08-09 02:47:45 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: https://dsa-practice-hub-frontend-0i71.onrender.com, 
+    origin: "https://dsa-practice-hub-frontend-0i71.onrender.com", 
     credentials: true, 
   }));
   
